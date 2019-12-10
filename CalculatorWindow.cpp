9e4c71bcee2d375910a032f0f6df7c65ba874f6f
Maks <klavioturasucka2@gmail.com>
2019-12-10 21:53:40 +0900

    ElectronicCalculator_0.1

---
+++ CalculatorWindow.cpp
@@ -0,0 +1,15 @@
+#include "CalculatorWindow.h"
+#include "ui_CalculatorWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
